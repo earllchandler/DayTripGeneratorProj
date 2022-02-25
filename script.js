ae9f295ee03768626d5f292f044a810645e38d71
Earl Chandler <earllchandler@gmail.com>
2022-02-25 15:59:52 -0500

    This is a function that returns a randominzed destination for the user

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+
+let destination = ["Aruba", "Spain", "Germany", "Jamacia"];
+
+function destinationTrip(choiceTrip) {
+  return choiceTrip[Math.floor(Math.random()*choiceTrip.length)];
+    
+
+}
+
+let tripChoice = destinationTrip(destination);
+
+//  alert(`
+//  Below is your randomly generated trip!!!   
+
+//         Destination: ${tripChoice}
+//         Resturant: ${tripChoice}
+//         Transportation: ${tripChoice}
+//         Entertainment: ${tripChoice}`); 
